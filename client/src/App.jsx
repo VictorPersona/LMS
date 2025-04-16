1c566ae1e326b71843e9dbbe12be040f4a2e2d74
VictorPersona <gauravtupe00@gmail.com>
2025-04-16 18:42:56 +0530

    implement routing and context provider; add navbar and loading component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/students/Home'
+import Courselist from './pages/students/Courselist'
+import CourseDetails from './pages/students/CourseDetails'
+import MyEnrollments from './pages/students/MyEnrollments'
+import Player from './pages/students/Player'
+import Loading from './pages/students/Loading'
+import Educator from './pages/educator/Educator'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import Students from './pages/educator/Students'
+import Enrolled from './pages/educator/Enrolled'
+import Navbar from './components/students/Navbar'
 
 const App = () => {
   return (
-    <div>
-      {' '}
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar/> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list/:input" element={<Courselist />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="students" element={<Students />} />
+          <Route path="enrolled" element={<Enrolled />} />
+          <Route path="educator" element={<Educator />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>Educator</div>
+    <div>
+      <div>{<Outlet />}</div>
+    </div>
   )
 }
 
